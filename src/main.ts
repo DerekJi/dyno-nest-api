51a55cd487e0f021ba87e75d076d7f08eb18c77c
Derek Ji <syfool@gmail.com>
2020-09-10 21:52:34 +0930

    feat: Setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('LDP API')
+    .setDescription('The LDP API description')
+    .setVersion('1.0')
+    .addTag('ldp')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+  
   await app.listen(3000);
 }
 bootstrap();
