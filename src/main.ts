469a9755a10644f74ae47ffc1d7c80e850522b85
Derek Ji <syfool@gmail.com>
2020-09-15 23:07:38 +0930

    feat(swagger): Configure Swagger to add 'Athorize' button

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,25 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { INestApplication } from '@nestjs/common';
 
+// const tenantId = 'bda528f7-fca9-432f-bc98-bd7e90d40906';
+const tenantId = process.env.TENANT_ID;
+const clientId = process.env.CLIENT_ID;
+
 function setupSwagger(app: INestApplication) {
   const options = new DocumentBuilder()
     .setTitle('SimpleApplicationService API')
     .setDescription('The SimpleApplicationService API description')
     .setVersion('v1')
-    // .addTag('SAS')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        implicit: {
+          scopes: { [`api://${clientId}/access_as_user`]: '' },
+          authorizationUrl: `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`,
+        },
+      },
+    })
+    // .addTag('SimpleApplicationService')
     .build();
     
   const document = SwaggerModule.createDocument(app, options);
