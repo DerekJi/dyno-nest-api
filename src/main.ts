88a47992ae51e07cf632eb7d0aa37a1b44842b88
Derek Ji <syfool@gmail.com>
2020-09-20 15:04:02 +0930

    feat: #4. User compression

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { INestApplication } from '@nestjs/common';
+import * as compression from 'compression';
 
 const tenantId = process.env.TENANT_ID;
 const clientId = process.env.CLIENT_ID;
-const authority = 'common';
 
 function setupSwagger(app: INestApplication) {
   const options = new DocumentBuilder()
@@ -17,7 +17,7 @@ function setupSwagger(app: INestApplication) {
       flows: {
         implicit: {
           scopes: { [`api://${clientId}/access_as_user`]: '' },
-          authorizationUrl: `https://login.microsoftonline.com/${authority}/oauth2/v2.0/authorize`,
+          authorizationUrl: `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`,
         },
       },
     })
@@ -31,6 +31,8 @@ function setupSwagger(app: INestApplication) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(compression());
+ 
   setupSwagger(app);
   
   await app.listen(3000);
