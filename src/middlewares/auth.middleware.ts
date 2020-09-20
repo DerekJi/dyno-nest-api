bf9a154653044853dbdf0c41ae35dabb0aeb4e36
Derek Ji <syfool@gmail.com>
2020-09-20 22:50:52 +0930

    feat: #3. Setup allowed URL (resoruces) to prevent invalid requests

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -8,15 +8,36 @@ export class AuthMiddleware implements NestMiddleware {
     process.env.USER_API_KEY,
   ];
 
+  protected readonly allowedOriginalUrls: string[] = [
+    'USER',
+    'TASK'
+  ];
+
   use(req: Request, res: Response, next: NextFunction) {
+    
+    const originalUrl = req.originalUrl.replace(/\?.*$/g, '');
     const apiKey: string = req.headers['x-api-key'] as string;
-    if (this.validateApiKey(apiKey)) {
-      next();
-    } else {
-      console.error(`Authentication failed`);
-      throw new UnauthorizedException();
+
+    if (!this.validateApiKey(apiKey)) {
+      throw new UnauthorizedException(`Authentication failed: incorrect api-key`);
+    } else if (!this.validateOriginalUrl(originalUrl)) {
+      throw new UnauthorizedException(`Authentication failed: invalid request`);
     }
 
+    next();
+  }
+
+  /**
+   * Indicates if the original url is allowed or not
+   * 
+   * @param originalUrl The original url of the request
+   */
+  protected validateOriginalUrl(originalUrl: string): boolean {
+    const valid = (originalUrl && 
+      this.allowedOriginalUrls.some(url => originalUrl.toLowerCase().endsWith(url.toLowerCase()))
+    );
+    
+    return valid;
   }
 
   /**
